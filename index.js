af49a5e6f0305124e5ada0921c5c9fc1262c8e42
Karin Hsin <karinhsin@gmail.com>
2021-09-06 16:21:13 +0800

    修改404頁面範例

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const express = require('express');
 // 2. 建立 web server 物件
 const app = express();
 
-// 3. 路由定義
+// 3. 路由定義開始
 app.get('/', (req, res)=>{
     res.send(`<h2>Hello World!</h2>`);
 });
-//這個路由只能透過get的方法 後面是路徑
+//這個路由只能透過get的方法 後面是路徑 路由定義結束
+
+app.use((req, res) => {
+    res.status(404).send(`<h1>找不到頁面</h1>`)
+})
 
 //較保險的設定 如果沒有的話就跑3000
 let port = process.env.PORT || 3000;  
