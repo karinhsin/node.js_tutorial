3545eaa98b27462b1cef4112e4130a1da1ec17be
Karin Hsin <karinhsin@gmail.com>
2021-09-06 10:58:52 +0800

    修改require/module.exports

--- practice/arrow-func.js
+++ practice/arrow-func.js
@@ -1,7 +1,8 @@
 const f1 = a=>a*a;
-const f3 = a=>a*a;  //f3沒有被放在module 其他檔案無法使用f3
+const f3 = a=>a*a*a;  //f3沒有被放在module 其他檔案無法使用f3
 
-console.log(f1(7));
-console.log( '1:',__dirname);
+//console.log(f1(7));
+//console.log( '1:',__dirname);
 
-module.exports = f1; //匯出 沒有放在這邊的常數變數都不會被匯出
+//module.exports = f1; //匯出 沒有放在這邊的常數變數都不會被匯出
+module.exports = {f1,f3}; //ES6語法 一般都是匯出obj 比較少匯出array 
--- practice/require01.js
+++ practice/require01.js
@@ -1,6 +1,11 @@
-const f1 = require('./arrow-func'); //匯入時也會執行匯入的內容 寫法一
-const f2 = require(__dirname + '/arrow-func'); //較傳統的寫法 寫法二
+const { f1, f3 } = require('./arrow-func'); //匯入時也會執行匯入的內容 通常都用這種 寫法一
+const f2 = require(__dirname + '/arrow-func'); //較傳統的寫法 寫法二 
+//f2接收整個物件 f2裡面有f1跟f3
 
 console.log('2:', __dirname);
 console.log(f1(9));
-console.log(f2(10));
+console.log(f3(10));
+
+console.log(f2.f1(5));
+console.log(f2.f3(5));
+//匯入匯出 參照的概念
