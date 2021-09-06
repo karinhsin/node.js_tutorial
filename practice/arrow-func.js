49bcf3efa088ec79b0994bddc115335195577ffa
Karin Hsin <karinhsin@gmail.com>
2021-09-06 10:47:17 +0800

    修改require/module.exports註解

--- practice/arrow-func.js
+++ practice/arrow-func.js
@@ -1,6 +1,7 @@
 const f1 = a=>a*a;
+const f3 = a=>a*a;  //f3沒有被放在module 其他檔案無法使用f3
 
 console.log(f1(7));
 console.log( '1:',__dirname);
 
-module.exports = f1; //匯出
+module.exports = f1; //匯出 沒有放在這邊的常數變數都不會被匯出
--- practice/require01.js
+++ practice/require01.js
@@ -1,5 +1,5 @@
-const f1 = require('./arrow-func'); //匯入時也會執行匯入的內容
-const f2 = require('./arrow-func');
+const f1 = require('./arrow-func'); //匯入時也會執行匯入的內容 寫法一
+const f2 = require(__dirname + '/arrow-func'); //較傳統的寫法 寫法二
 
 console.log('2:', __dirname);
 console.log(f1(9));
