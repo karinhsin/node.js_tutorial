a6de6dc16eb4ec98c52fc68fc048eb70009e2ce8
Karin Hsin <karinhsin@gmail.com>
2021-09-10 10:50:20 +0800

    address-book:總筆數

--- routes/address-book.js
+++ routes/address-book.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const db = require('./../modules/connect-mysql');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('address-book/main');
 });
+
+router.get('/list', async (req, res) => {
+    const perPage = 5;
+    let page = parseInt(req.query.page) || 1;
+    const output = {
+
+    };
+
+    const t_sql = "SELECT COUNT(1) totalRows FROM address_book ";
+    const [[{ totalRows }]] = await db.query(t_sql);
+    output.totalRows = totalRows;
+
+    res.json(output);
+    // res.render('address-book/list');
+});
+
 module.exports = router;
