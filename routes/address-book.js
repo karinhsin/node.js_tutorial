96ac5cf4b49e648f40299bfcd918f6cdb4db13e9
Karin Hsin <karinhsin@gmail.com>
2021-09-13 14:00:14 +0800

    address-book: 修改資料功能

--- routes/address-book.js
+++ routes/address-book.js
@@ -123,6 +123,25 @@ router.route('/edit/:sid')
             success: false,
             postData: req.body,
         }
+
+        const input = { ...req.body };
+        const sql = "UPDATE `address_book` SET ? WHERE sid=?";
+        let result = {};
+        // 處理修改資料時可能的錯誤
+        try {
+            [result] = await db.query(sql, [input, req.params.sid]);
+        } catch (ex) {
+            output.error = ex.toString();
+        }
+        output.result = result;
+        if (result.affectedRows === 1) {
+            if (result.changedRows === 1) {
+                output.success = true;
+            } else {
+                output.error = '資料沒有變更';
+            }
+        }
+
         res.json(output);
     });
     
