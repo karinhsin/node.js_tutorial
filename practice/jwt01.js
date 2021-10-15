3a6a93fc2cd8e56905fe1d3530bf40521a11b789
Karin Hsin <karinhsin@gmail.com>
2021-10-15 10:41:14 +0800

    sql data

--- practice/jwt01.js
+++ practice/jwt01.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 //key如果換了 token就要重來 否則無法解密之前加密的內容
-const secretKey = 'abc';
+const secretKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGF2aWQiLCJp';
 
 (async () => {
 
@@ -11,11 +11,11 @@ const secretKey = 'abc';
 
     console.log(token);
 
-    const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGF2aWQiLCJpYXQiOjE2MzE2ODYwNjN9.1S8SsBae9Nb8kLZaz4aBsFcLAzC1v4Hn-eDhyPXlGrg';
+    // const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGF2aWQiLCJpYXQiOjE2MzE2ODYwNjN9.1S8SsBae9Nb8kLZaz4aBsFcLAzC1v4Hn-eDhyPXlGrg';
 
-    // 解密
-    const decoded = await jwt.verify(token1, secretKey);
+    // // 解密
+    // const decoded = await jwt.verify(token1, secretKey);
 
-    console.log(decoded);
+    // console.log(decoded);
 
 })()
