6b8acc0fda2f12f7da8cd9510a82591333e58d7f
Karin Hsin <karinhsin@gmail.com>
2021-10-15 10:47:38 +0800

    cart: jwt驗證

--- routes/cart.js
+++ routes/cart.js
@@ -1,68 +1,40 @@
 const express = require('express');
-//const db = require('./../modules/connect-mysql');
-const upload = require('../modules/upload-images');
-const Product = require('../models/Product');
+
+const Cart = require('./../models/Cart');
+const { getListData } = require("./address-book");
 
 const router = express.Router();
 
-// 列表
+router.use((req, res, next) => {
+    // 判斷有沒有通過 jwt 驗證 (寫在 index.js 主程式)
+    if (req.myAuth && req.myAuth.id) {
+        next();
+    } else {
+        res.json({ success: false, error: '沒有 token 或者 token 不合法' });
+    }
+});
+
+// 讀取購物車清單
 router.get('/', async (req, res) => {
-    res.json(await Product.findAll());
+    res.json({ info: 'ok' });
 });
 
-// 讀取單筆 async後面都是路由處理器
-//改寫 把資料處理的部分交給models那邊 這邊程式碼會看起來比較單純 較好管理
-router.get('/:id', async (req, res) => {
-    const output = {
-        success: false,
-        data: null,
-    };
-    output.data = await Product.findOne(req.params.id);
-    if (output.data) {
-        output.success = true;
-    }
-    res.json(output);
-});
-
-// 測試用
-router.get('/test01/2', async (req, res) => {
-
-    const p1 = await Product.findOne(2);
-    p1.data.price *= 2;
-
-    res.json(await p1.save());
-});
-// TODO: 管理的功能, 需要登入後才能操作
-// 新增
+// 新增項目
 router.post('/', async (req, res) => {
-    const p1 = new Product(req.body);
-    res.json(await p1.save());
+
 });
 
-// 修改
+// 修改項目
 router.put('/:id', async (req, res) => {
-    const output = {
-        success: false,
-        result:null,
-    };
-    const p1 = await Product.findOne(req.params.id);
-    //先去找到那筆
-    if(p1){ 
-        //如果有東西才去做修改的動作
-        output.success = true;
-        output.result = await p1.edit(req.body);
-    }
-    res.json(output);
-});
-//到postman>PUT>輸入路徑>body>raw>JSON>輸入要修改的資料後送出
 
-// 刪除
-router.delete('/:id', async (req, res) => {
-    const p1 = await Product.findOne(req.params.id);
-    if (p1) {
-        return res.json(await p1.remove());
-    }
-    res.json({ info: 'item not found!' });
 });
+
+// 刪除項目
+router.delete('/:id', async (req, res) => {
+
+});
+
+// 清空購物車
+
 
 module.exports = router;
